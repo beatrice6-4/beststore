0b35a2db26ed0424c12601293b81012a1d89bf27
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-15 11:19:53 +0300

    make it better

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('place_order/', views.place_order, name='place_order'),
     path('payments/', views.payments, name='payments'),
     path('order_complete/', views.order_complete, name='order_complete'),
-    path('mpesa_payment/<str:order_number>/', views.mpesa_payment, name='mpesa_payment'),
+    path('access_token/', views.get_access_token, name='get_access_token'),
+    path('stkpush/', views.initiate_stk_push, name='initiate_stk_push'),
 ]
