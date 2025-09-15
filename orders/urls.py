288d50655dd5fcfc889138ce8b86085b645b8dd2
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-15 11:28:49 +0300

    make it better

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('order_complete/', views.order_complete, name='order_complete'),
     path('access_token/', views.get_access_token, name='get_access_token'),
     path('stkpush/', views.initiate_stk_push, name='initiate_stk_push'),
+    path('query/', views.query_stk_status, name='query_stk_status')
 ]
