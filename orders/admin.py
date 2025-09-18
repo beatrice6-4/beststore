5d6f193e19c2dd6dbd79212e5e9bfc88e6a2ec60
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-18 10:13:54 +0300

    make it better

--- orders/admin.py
+++ orders/admin.py
@@ -13,6 +13,16 @@ class OrderProductInline(admin.TabularInline):
 #    readonly_fields = ('payment_id', 'payment_method', 'amount_paid', 'status', 'created_at')
 #    extra = 0
 
+
+from django.contrib import admin
+from .models import Payment
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_id', 'payment_method', 'amount_paid', 'status', 'created_at')
+    search_fields = ('user__email', 'payment_id', 'payment_method')
+    list_filter = ('status', 'payment_method', 'created_at')
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
     list_filter = ['status', 'is_ordered']
@@ -20,6 +30,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_per_page = 20
     inlines = [OrderProductInline]
 
-admin.site.register(Payment)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderProduct)
