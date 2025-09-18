f827cffccaf87a14ff274f221a66708b89ed4290
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-18 11:17:44 +0300

    make it better

--- orders/query.py
+++ orders/query.py
@@ -12,7 +12,7 @@ def query_stk_status(request):
         access_token_json = json.loads(access_token)
         access_token = access_token_json.get('access_token')
         if access_token:
-            query_url = 'https://mpesa-requester.safaricom.co.ke/mpesa/stkpushquery/v1/query'
+            query_url = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
             business_short_code = '5429863'
             timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
             passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
--- orders/stkPush.py
+++ orders/stkPush.py
@@ -37,7 +37,7 @@ def initiate_stk_push(request, order_number):
         # Prepare STK Push parameters for live payments
         passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         business_short_code = '5429863'
-        process_request_url = 'https://mpesa-requester.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+        process_request_url = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
         callback_url = 'https://mamamaasaibakers.com/orders/mpesa/callback/'
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         password = base64.b64encode((business_short_code + passkey + timestamp).encode()).decode()
