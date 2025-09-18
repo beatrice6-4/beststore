ac8ab880256b0c84934ccf15fc53fd7b81b2ac0c
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-18 17:38:17 +0300

    make it better

--- accounts/views.py
+++ accounts/views.py
@@ -230,7 +230,7 @@ from orders.models import Order
 
 @login_required(login_url='login')
 def my_orders(request):
-    orders = Order.objects.filter(user=request.user, is_ordered=True, payment__status="Completed").order_by('-created_at')
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
     context = {
         'orders': orders,
     }
