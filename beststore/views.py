e017e280397e1fc36eb23a7e82e5976c205e4580
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-19 09:46:39 +0300

    make it better

--- beststore/views.py
+++ beststore/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from store.models import Product
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def home(request):
     products = Product.objects.all().filter(is_available=True)  # Fetch all available products rom the database
 
