daa562164fc8cc407baa69ca700ee9e71fe58bbd
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-21 09:17:57 +0300

    make it better

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ from store.models import Product, Variation
 
 class Payment(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    payment_id = models.CharField(max_length=100, null=True, blank=True)
+    payment_id = models.CharField(max_length=100, null=True, blank=True)  # <-- Ensure this is set
     reference_code = models.CharField(max_length=20, unique=True, blank=True, null=True)
     payment_method = models.CharField(max_length=100)
     amount_paid = models.CharField(max_length=100)
