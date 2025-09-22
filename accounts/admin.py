40f3d61dac78028cf9a404410fd208733130a088
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-22 14:46:58 +0300

    make better

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, ContactMessage
 from django.utils.html import format_html
 
+
 # Register your models here.
+
+
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subject', 'created_at', 'is_read')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('subject', 'message')
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
 
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
