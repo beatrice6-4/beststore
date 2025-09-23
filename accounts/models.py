b59cd95c174274155b66fe45197edddc9f0085cc
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-23 13:45:00 +0300

    make it better

--- accounts/models.py
+++ accounts/models.py
@@ -101,10 +101,12 @@ from django.conf import settings
 
 class ContactMessage(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=255)
+    subject = models.CharField(max_length=40)
+    email = models.EmailField(max_length=100)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
+    is_read = models.BooleanField(default=True)
+
 
     def __str__(self):
         return f"{self.subject} from {self.user.email}"
