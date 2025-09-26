27bb966d8df05117d3bec9c39f28c5798e249425
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-26 11:37:20 +0300

    make it better

--- CDMIS/admin.py
+++ CDMIS/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Group, Payment, Activity, Training, Service, Finance
+from .models import Group, Payment, Activity, Training, Service
 
 admin.site.register(Group)
 admin.site.register(Payment)
-admin.site.register(Finance)
 admin.site.register(Activity)
 admin.site.register(Training)
 admin.site.register(Service)
