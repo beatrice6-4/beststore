67866d46a9d889473f58b8938d2101e8b8218e6f
bramwel <bramwelwanyonyi2021@gmail.com>
2025-09-30 10:53:19 +0300

    make it better

--- CDMIS/views.py
+++ CDMIS/views.py
@@ -221,12 +221,14 @@ class FinanceView(UserPassesTestMixin, View):
 
 
 
-from django.contrib.auth.models import User
+
 from django.views.generic import ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class UserListView(UserPassesTestMixin, ListView):
     model = User
