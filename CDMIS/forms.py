9152bf911f18857f65efd11275688e5f62cbbfa7
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-01 13:42:24 +0300

    make it better

--- CDMIS/forms.py
+++ CDMIS/forms.py
@@ -5,3 +5,16 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['user', 'product', 'amount', 'status']
+
+from django import forms
+from .models import Group
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name', 'registration_date', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'registration_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
