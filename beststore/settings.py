9190dcc7f413e3720c74e2d6b707c8248e744cca
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-02 08:24:25 +0300

    make it better

--- beststore/settings.py
+++ beststore/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['mamamaasaibakers.com' ]
 
 INSTALLED_APPS = [
-    
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
