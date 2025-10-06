8c01289ec35bfe13e4c4729ef77a5ce1558c3efc
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-06 10:17:09 +0300

    make it better

--- beststore/settings.py
+++ beststore/settings.py
@@ -119,12 +119,7 @@ WSGI_APPLICATION = 'beststore.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.Account'
 
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
