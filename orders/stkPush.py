0457ea9ee489cf0cd4c1450810b4d77daef1a9fa
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-09 09:23:41 +0300

    make it better

--- orders/stkPush.py
+++ orders/stkPush.py
@@ -36,8 +36,8 @@ def initiate_stk_push(request, order_number):
 
         # Prepare STK Push parameters for sandbox payments
         passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-        business_short_code = '174379'  # Sandbox Short Code
-        process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+        business_short_code = '600986'
+        process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl'
         callback_url = 'https://your-dev-callback-url.com/orders/mpesa/callback/'  # Use a test callback URL
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         password = base64.b64encode((business_short_code + passkey + timestamp).encode()).decode()
