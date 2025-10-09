49b46c6912edbb0838da16346ec58c97f306670a
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-09 16:32:16 +0300

    make it better

--- orders/stkPush.py
+++ orders/stkPush.py
@@ -36,7 +36,7 @@ def initiate_stk_push(request, order_number):
 
         # Prepare STK Push parameters for sandbox payments
         passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-        business_short_code = '6391014'  # <-- Use your actual Till Number here
+        business_short_code = '174379'  # <-- Use your actual Till Number here
         process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
         callback_url = 'https://mamamaasaibakers.com/orders/mpesa/callback/'
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
