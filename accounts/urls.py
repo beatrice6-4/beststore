596187b65fc15b04081d1f2f16011a9290a6fb36
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-13 15:35:56 +0300

    make it better

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,65 +1,70 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('', views.dashboard, name='dashboard'),
-    path('customerDashboard/', views.customerDashboard, name='customerDashboard'),
-    path('userManagement/', views.user_management, name='user_management'),
-    path('track_order/<int:order_id>/', views.track_order, name='track_order'),
-    path('transactions/', views.transactions, name='transactions'),
-    path('recipes/', views.recipes, name='recipes'),
-    path('contact/', views.contact, name='contact'),
-    path('wishlist/', views.wishlist, name='wishlist'),
-    path('about/', views.about, name='about'),
-    path('account/', views.account, name='account'),
-    path('products/', views.products, name='products'),
-    path('profile/', views.profile, name='profile'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('categories/', views.category_list, name='categories'),
-    path('add_category/', views.add_category, name='add_category'),
-    path('users/', views.users_view, name='users'),
 
-    path('dashboard/', views.finance_dashboard, name='finance_dashboard'),
+    # General dashboards
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('customerDashboard/', views.customerDashboard, name='customerDashboard'),
+
+    # Finance
+    path('finance_dashboard/', views.finance_dashboard, name='finance_dashboard'),
     path('add-payment/', views.add_payment, name='add_payment'),
-    path('users/', views.user_management, name='user_management'),
+
+    # User management
+    path('userManagement/', views.user_management, name='user_management'),
+    path('users/', views.users_view, name='users'),
     path('users/add/', views.add_user, name='add_user'),
     path('users/<int:user_id>/', views.user_detail, name='user_detail'),
 
+    # Categories / products
+    path('categories/', views.category_list, name='categories'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('products/', views.products, name='products'),
 
+    # Orders / payments / carts
+    path('myOrders/', views.myOrders, name='myOrders'),
+    path('order_detail/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('orders/', views.order_list, name='order_list'),
+    path('payments/', views.payment_list, name='payment_list'),
+    path('carts/', views.cart_list, name='cart_list'),
+    path('cart-items/', views.cart_items, name='cart_items'),
 
+    # Account/profile
+    path('account/', views.account, name='account'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
+
+    # Auth helpers
+    path('redirect-after-login/', views.redirect_after_login, name='redirect_after_login'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
+
+    # Misc
     path('contact/', views.contact, name='contact'),
-
-    path('myOrders/', views.myOrders, name='myOrders'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('change_password/', views.change_password, name='change_password'),
-    path('order_detail/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('redirect-after-login/', views.redirect_after_login, name='redirect_after_login'),
-
-
-
-
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    
-    path('carts/', views.cart_list, name='cart_list'),
-    path('cart-items/', views.cart_items, name='cart_items'),
-    path('categories/', views.category_list, name='category_list'),
-    path('orders/', views.order_list, name='order_list'),
-    path('payments/', views.payment_list, name='payment_list'),
-    path('products/', views.product_list, name='product_list'),
-    path('variations/', views.variation_list, name='variation_list'),
     path('contact-messages/', views.contact_messages, name='contact_messages'),
-    # CDMIS
+    path('transactions/', views.transactions, name='transactions'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('about/', views.about, name='about'),
+    path('recipes/', views.recipes, name='recipes'),
+
+    # Admin lists
+    path('product_list/', views.product_list, name='product_list'),
+    path('variation_list/', views.variation_list, name='variation_list'),
+    path('payment_list/', views.payment_list, name='payment_list'),
+    path('category_list/', views.category_list, name='category_list'),
+    path('contact_messages/', views.contact_messages, name='contact_messages'),
+
+    # CDMIS / other lists
     path('groups/', views.group_list, name='group_list'),
     path('activities/', views.activity_list, name='activity_list'),
     path('services/', views.service_list, name='service_list'),
-    path('trainings/', views.training_list, name='training_list')
+    path('trainings/', views.training_list, name='training_list'),
 ]
