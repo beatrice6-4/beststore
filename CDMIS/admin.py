04381ac0de69b34a54b79a604e89a66bc9e93151
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-13 09:12:25 +0300

    make it better

--- CDMIS/admin.py
+++ CDMIS/admin.py
@@ -39,6 +39,9 @@ class RequirementAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title',)
 
-
-
-
+from django.contrib import admin
+from .models import Document
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_by', 'uploaded_at')
+    search_fields = ('title',)
