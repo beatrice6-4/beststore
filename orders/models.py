e091602aa809ced12ae8f804fc5c3cbf10c8153f
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-24 08:28:45 +0300

    Initial commit

--- orders/models.py
+++ orders/models.py
@@ -12,7 +12,7 @@ class Payment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.payment_id if self.payment_id else "No Mpesa Code"
+        return self.payment_id if self.payment_id else "No Code"
 
 class Order(models.Model):
     STATUS = (
@@ -43,9 +43,9 @@ class Order(models.Model):
     order_note = models.CharField(max_length=100, blank=True)
     order_total = models.FloatField()
     tax = models.FloatField()
-    status = models.CharField(max_length=10, choices=STATUS, default='Pending')
+    status = models.CharField(max_length=10, choices=STATUS, default='Accepted')
     ip = models.CharField(blank=True, max_length=20)
-    is_ordered = models.BooleanField(default=True)
+    is_ordered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
