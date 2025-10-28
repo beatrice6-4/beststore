35e683fe507ea9e559eb88cfd6a80ec1790301a0
bramwel <bramwelwanyonyi2021@gmail.com>
2025-10-28 09:53:29 +0300

    Initial commit

--- orders/stkPush.py
+++ orders/stkPush.py
@@ -30,7 +30,7 @@ def initiate_stk_push(request, order_number):
         passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         business_short_code = "174379"  # PayBill number
         account_reference = "600996"  # Account number
-        process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl'
+        process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
         callback_url = 'https://mamamaasaibakers.com/orders/mpesa/callback/'
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         password = base64.b64encode((business_short_code + passkey + timestamp).encode()).decode()
