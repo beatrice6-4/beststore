846ea64626fc42d59b256000fe2c7a04ba4e1787
bramwel <bramwelwanyonyi2021@gmail.com>
2025-11-03 11:09:18 +0300

    make it better

--- orders/generateAcesstoken.py
+++ orders/generateAcesstoken.py
@@ -1,18 +1,14 @@
 import requests
-from django.http import JsonResponse
 
-def get_access_token(request):
-    consumer_key = "kfy4wKPlLpC4AGlZRphJl1VgpzAVc1fNVs4w2l3vNVb1GuqM"  
-    consumer_secret = "4TgAMbmpG5wbH4mqFpIYZ6tOCGFWGnJjMBvFE7Z4DobFfcJjmvDjLnsWcoIm9FLf"  
-    access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate'
-    headers = {'Content-Type': 'application/json'}
-    auth = (consumer_key, consumer_secret)
+def get_access_token():
+    consumer_key = "kfy4wKPlLpC4AGlZRphJl1VgpzAVc1fNVs4w2l3vNVb1GuqM"
+    consumer_secret = "4TgAMbmpG5wbH4mqFpIYZ6tOCGFWGnJjMBvFE7Z4DobFfcJjmvDjLnsWcoIm9FLf"
+    access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
     try:
-        response = requests.get(access_token_url, headers=headers, auth=auth)
-        response.raise_for_status() 
+        response = requests.get(access_token_url, auth=(consumer_key, consumer_secret))
+        response.raise_for_status()
         result = response.json()
-        access_token = result['access_token']
-        return JsonResponse({'access_token': access_token})
+        return result.get('access_token')
     except requests.exceptions.RequestException as e:
-        return JsonResponse({'error': str(e)})
-    
+        print(f"Access token error: {e}")
+        return None
