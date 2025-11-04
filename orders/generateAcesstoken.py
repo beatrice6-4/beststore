76212f575477aa93d5edf66626fab4e467a57627
bramwel <bramwelwanyonyi2021@gmail.com>
2025-11-04 09:41:50 +0300

    make it better

--- orders/generateAcesstoken.py
+++ orders/generateAcesstoken.py
@@ -1,10 +1,10 @@
 import requests
 from django.http import JsonResponse
 
-def get_access_token(request):
-    consumer_key = "Y5yB22NEdfA7h1tEzr3d0bPKj2SG6p3v48wD9HgdBo8ABM0W"  
-    consumer_secret = "rGAb1Ic3OcdyDtvHOUyjJiq301cjTiMmzczDPQTkG3T5YQrpZkfdrfL34p8CpPST"  
-    access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+def get_access_token():
+    consumer_key = "kfy4wKPlLpC4AGlZRphJl1VgpzAVc1fNVs4w2l3vNVb1GuqM"  
+    consumer_secret = "4TgAMbmpG5wbH4mqFpIYZ6tOCGFWGnJjMBvFE7Z4DobFfcJjmvDjLnsWcoIm9FLf"  
+    access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
     headers = {'Content-Type': 'application/json'}
     auth = (consumer_key, consumer_secret)
     try:
