d7a4f78338466164511aab0eec0477acd964d9d4
bramwel <bramwelwanyonyi2021@gmail.com>
2025-11-04 17:23:05 +0300

    make it better

--- orders/generateAcesstoken.py
+++ orders/generateAcesstoken.py
@@ -1,17 +1,9 @@
 import requests
 
 def get_access_token():
-    consumer_key = "kfy4wKPlLpC4AGlZRphJl1VgpzAVc1fNVs4w2l3vNVb1GuqM"  
-    consumer_secret = "4TgAMbmpG5wbH4mqFpIYZ6tOCGFWGnJjMBvFE7Z4DobFfcJjmvDjLnsWcoIm9FLf"  
-    access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-    headers = {'Content-Type': 'application/json'}
-    auth = (consumer_key, consumer_secret)
-    try:
-        response = requests.get(access_token_url, headers=headers, auth=auth)
-        response.raise_for_status() 
-        result = response.json()
-        access_token = result['access_token']
-        return access_token
-    except requests.exceptions.RequestException as e:
-        print("Access token error:", str(e))
-        return None
+    consumer_key = "kfy4wKPlLpC4AGlZRphJl1VgpzAVc1fNVs4w2l3vNVb1GuqM"
+    consumer_secret = "4TgAMbmpG5wbH4mqFpIYZ6tOCGFWGnJjMBvFE7Z4DobFfcJjmvDjLnsWcoIm9FLf"
+    url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    response = requests.get(url, auth=(consumer_key, consumer_secret))
+    access_token = response.json()['access_token']
+    return access_token
