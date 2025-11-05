52f0ecf4eff5646fe88d906689ea7ea4b96f565f
bramwel <bramwelwanyonyi2021@gmail.com>
2025-11-05 10:16:05 +0300

    make it better

--- orders/views.py
+++ orders/views.py
@@ -103,7 +103,7 @@ def mpesa_payment(request, order_number):
                     payment_response = {'error': f'Invalid phone number format ({original_phone}). Use 2547XXXXXXXX.'}
                 else:
                     passkey = "46c4b4ea9885ebebe4054aa05ba24ebede63a956de7286c28135be035bdec933"  # LIVE passkey
-                    business_short_code = '3581517'  # LIVE shortcode
+                    business_short_code = '6391014'  # LIVE shortcode
                     process_request_url = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
                     callback_url = 'https://mamamaasaibakers-917922e1976b.herokuapp.com/orders/mpesa/callback/'
                     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
