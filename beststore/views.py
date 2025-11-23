0286dc4134a5d797d7c37dff8e5cdb74a677f316
bramwel <bramwelwanyonyi2021@gmail.com>
2025-11-23 08:28:15 +0300

    make it better

--- beststore/views.py
+++ beststore/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from store.models import Product
 from django.contrib.auth.decorators import login_required
 
-def home(request):
+def dashboard(request):
     products = Product.objects.all().filter(is_available=True)  # Fetch all available products rom the database
 
 
     context = {'products': products}
 
-    return render(request, 'home.html', context)
+    return render(request, 'dashboard.html', context)
 
 
 
